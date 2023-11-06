fe34be54bc777a2a6a0d4598d7c72e6fee4e061e
Jason Gray <103676092+Muppetpants@users.noreply.github.com>
2023-11-06 09:49:55 -0500

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Description: Create lists of random MAC Addresses, redirects output to specified file name.
-#Usage: ./mac.sh <Countds>
+#Usage: ./mac.sh <Count>
 
 if [ $# -eq 0 ]
   then
